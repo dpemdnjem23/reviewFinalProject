a8a150644d87c1eb2cd7f6a3a3679cdf727ea1da
gitkrak <dpemdnjem23@gamail.com>
2022-01-09 00:16:50 +0900

    master

---
+++ server/index.js
@@ -0,0 +1,58 @@
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const https = require("https");
+// const multer = require("multer");
+// const upload = multer({dest: "uploads/"});
+
+const DB = require("./config/config");
+const boardRouter = require("./routes/board");
+const userRouter = require("./routes/user");
+const authRouter = require("./routes/auth");
+const commentRouter = require("./routes/comment");
+const mailRouter = require("./routes/mail");
+const mapRouter = require("./routes/map");
+const imageRouter = require("./routes/image");
+require("dotenv").config();
+//use modules
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static("public"));
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true,
+  }),
+);
+app.use(cookieParser());
+DB();
+app.get("/", (req, res) => {
+  res.status(200).send("hello world....!!");
+});
+
+//routes
+
+app.use("/user", userRouter);
+app.use("/auth", authRouter);
+app.use("/board", boardRouter);
+app.use("/comment", commentRouter);
+app.use("/mail", mailRouter);
+app.use("/map", mapRouter);
+app.use("/image", imageRouter);
+
+
+
+//server
+const HTTPS_PORT = 8080
+
+//인증서 없는경우
+
+app.listen(HTTPS_PORT, () => {
+  console.log(`      🚀 Server is starting on http ${HTTPS_PORT}`);
+});
+
+module.exports = app;
---
+++ server/routes/comment.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const controller = require("../controllers/comment");
+
+
+//freeboardview
+router.
+// freeboard commnent
+router.post("/fbcommentregister", controller.fbcommentregisterControl);
+router.patch("/fbcommentedit", controller.fbcommenteditControl);
+router.delete("/fbcommentdelete", controller.fbcommentdeleteControl);
+// freeboard child commnent
+router.post("/fbchildregister", controller.fbchildregisterControl);
+router.patch("/fbchildedit", controller.fbchildeditControl);
+router.delete("/fbchilddelete", controller.fbchilddeleteControl);
+
+//crewboard comment
+router.post("/cbcommentregister", controller.cbcommentregisterControl);
+router.patch("/cbcommentedit", controller.cbcommenteditControl);
+router.delete("/cbcommentdelete", controller.cbcommentdeleteControl);
+// crewboard child commnent
+router.post("/cbchildregister", controller.cbchildregisterControl);
+router.patch("/cbchildedit", controller.cbchildeditControl);
+router.delete("/cbchilddelete", controller.cbchilddeleteControl);
+
+module.exports = router;
