37364a4360214eef797cf297f8bf02c8cb49da62
gitkrak <dpemdnjem23@gmail.com>
2022-03-04 22:12:58 +0900

    comment edit

--- server/controllers/comment.ts
+++ server/controllers/comment.ts
@@ -1,6 +1,7 @@
 export {};
 
 import express from "express";
+import { stringify } from "querystring";
 
 import { BoardFree, Userdata } from "../inteface";
 require("dotenv").config();
@@ -12,10 +13,7 @@ const Child_comment = require("../models/Freechildcomment");
 const { isAuthorized } = require("../middlewares/token");
 
 module.exports = {
-  fbcommentControl: async (
-    req: express.Request,
-    res: express.Response
-  ) => {
+  fbcommentControl: async (req: express.Request, res: express.Response) => {
     try {
       //회원만 댓글을 달수 있다.
       //
@@ -35,15 +33,18 @@ module.exports = {
       if (!fb) {
         return res.status(400).send("게시판 들어가서 댓글써야함");
       }
- //save는 수정도 가능함
-      const freeComment:{user_id:string,freeboard_id:string,commnet:string} = await Comment.create({
+      //save는 수정도 가능함
+      const freeComment: {
+        user_id: string;
+        freeboard_id: string;
+        commnet: string;
+      } = await Comment.create({
         user_id: userData.user_id,
         freeboard_id: freeboard_id,
         comment: comment,
-      })
+      });
 
-return res.status(201).send(freeComment)
-
+      return res.status(201).send(freeComment);
     } catch (err) {
       console.log(err);
       return res.status(500).send(err);
@@ -52,6 +53,26 @@ return res.status(201).send(freeComment)
 
   fbcommenteditControl: async (req: express.Request, res: express.Response) => {
     try {
+
+      const {
+        freeboard_id,
+        comment,
+      }: { freeboard_id: string; comment: string } = req.body;
+
+      const userData: { user_id: string } = isAuthorized(req, res);
+
+      if (!userData) {
+        return res.status(401).send("회원가입 필요");
+      }
+
+      const freeCommentEdit: {
+        user_id: string;
+        freeboard_id: string;
+        comment: string;
+      } = await Comment.findOneAndUpdate({ user_id: userData.user_id , _id:freeboard_id },{comment:comment});
+
+
+      return res.status(200).send(freeCommentEdit)
     } catch (err) {
       console.log(err);
       return res.status(500).send(err);
