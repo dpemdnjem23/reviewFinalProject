dd113d589abc1a05453fb7f208b33006b35c5a41
gitkrak <dpemdnjem23@gmail.com>
2022-03-11 20:19:30 +0900

    child edit 완료

--- server/controllers/comment.ts
+++ server/controllers/comment.ts
@@ -133,7 +133,7 @@ module.exports = {
         childcomment,
         freecomment_id,
       }: {
-        parent_id:string;
+        parent_id: string;
         freeboard_id: string;
         childcomment: string;
         freecomment_id: string;
@@ -144,38 +144,33 @@ module.exports = {
       if (!userData) {
         return res.status(401).send("회원가입 필요");
       }
-      
+
       const commentinchild: {
         freeboard_id: string;
         user_id: string;
         freecommnet_id: string;
         child_comment: string;
-      } =
-        await Comment.findOneAndUpdate(
-          {
-            _id: freecomment_id,
-            freeboard_id: freeboard_id,
+      } = await Comment.findOneAndUpdate(
+        {
+          _id: freecomment_id,
+          freeboard_id: freeboard_id,
+        },
+        {
+          $push: {
+            freechildcomments: [
+              {
+                child_comment: childcomment,
+                user_id: userData.user_id,
+                parent_id: parent_id, //역할이 결국 parent역할 parent가 a라고하면
+              },
+            ],
           },
-          {
-            $push: {
-              freechildcomments: [
-                {
-                  
-                  child_comment: childcomment,
-                  user_id: userData.user_id,
-                  parent_id: parent_id, //역할이 결국 parent역할 parent가 a라고하면 
-         
-                  
-                },
-                
-              ]
-            }
-          },{upsert:true}
-        )
+        },
+        { upsert: true }
+      );
 
-       //대댓글 밑에 대댓글 달기 => depth =1
+      //대댓글 밑에 대댓글 달기 => depth =1
 
-        
       //대댓글 생성 ,
       console.log(commentinchild);
       // const commentinchilecomment: { freecomment_id: string } =
@@ -199,33 +194,39 @@ module.exports = {
       //회원만 댓글을 달수 있다.
       //
       const {
-        freeboard_id,
-        comment,
-      }: { freeboard_id: string; comment: string } = req.body;
+        child_comment_id,
+        parent_id,
+        childcomment,
+      }: {
+        child_comment_id: string;
+        parent_id: string;
+        childcomment: string;
+      } = req.body;
 
       const userData: { user_id: string } = isAuthorized(req, res);
 
       if (!userData) {
         return res.status(401).send("회원가입 필요");
       }
-      const fb: { _id: string } = await Freeboard.findById(freeboard_id);
-      console.log(fb);
-      //댓글에 필요한것 작성 날짜, 유저이름, 댓글 내용
-      if (!fb) {
-        return res.status(400).send("게시판 들어가서 댓글써야함");
-      }
-      //save는 수정도 가능함
-      const freeComment: {
+      //childcomment 수정 하려면 child를 찾아서 수정만하면된다.
+      const commentinchild: {
+        _id: string;
         user_id: string;
-        freeboard_id: string;
-        commnet: string;
-      } = await Comment.create({
-        user_id: userData.user_id,
-        freeboard_id: freeboard_id,
-        comment: comment,
-      });
+        parent_id: string;
+        childcomment: string;
+      } =
+        //child,user_id ,parent가 일치하는 child를 찾아 comment를 바꾼다.
+        await Child_comment.findOneAndUpdate(
+          {
+            _id: child_comment_id,
+            user_id: userData.user_id,
+            parent_id: parent_id,
+          },
+          { childcomment: childcomment }
+        );
+      console.log(commentinchild);
 
-      return res.status(201).send(freeComment);
+      return res.status(200).send(commentinchild);
     } catch (err) {
       console.log(err);
       return res.status(500).send(err);
@@ -235,23 +236,30 @@ module.exports = {
     try {
       //comment 삭제시 대댓글이 다지워지므로, child나 comment가 있을시 댓글은 삭제되지 않는다. isdeleted true로 댓글을 숨겨줌
 
-            //
+      //
       const {
         parent_id,
         childcomment_id,
         freeboard_id,
         comment,
-      }: { freeboard_id: string; comment: string, childcomment_id:string, parent_id:string } = req.body;
+      }: {
+        freeboard_id: string;
+        comment: string;
+        childcomment_id: string;
+        parent_id: string;
+      } = req.body;
 
       const userData: { user_id: string } = isAuthorized(req, res);
 
       if (!userData) {
         return res.status(401).send("회원가입 필요");
       }
-      const deletechild = await Child_comment.findByIdAndUpdate({parent_id,childcomment_id},{isDeleted:true})
-     
+      const deletechild = await Child_comment.findByIdAndUpdate(
+        { parent_id, childcomment_id },
+        { isDeleted: true }
+      );
+
       //save는 수정도 가능함
-     
 
       return res.status(201).send(deletechild);
     } catch (err) {
